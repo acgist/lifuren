b24e9459c1300d7f8bec47c2077b9d2a7b9269f9
acgist <289547414@qq.com>
2023-06-16 22:26:49 +0800

    [+] actions

--- xx/glog/src/GLog.cpp
+++ xx/glog/src/GLog.cpp
@@ -1,7 +1,10 @@
-#include <iostream>
+#include <filesystem>
+
 #include "glog/logging.h"
 
 int main(int argc, char const *argv[]) {
+    std::filesystem::create_directories("logs");
+    FLAGS_v                = 1;    // 输出级别
     FLAGS_alsologtostderr  = true; // 文件和控制台
     FLAGS_colorlogtostderr = true; // 颜色输出
     google::SetLogDestination(google::GLOG_INFO,    "logs/info_");
@@ -9,11 +12,14 @@ int main(int argc, char const *argv[]) {
     google::SetLogDestination(google::GLOG_ERROR,   "logs/error_");
     google::SetLogDestination(google::GLOG_FATAL,   "logs/fatal_");
     google::SetLogFilenameExtension(".log");
-    google::InitGoogleLogging("lifuren");
+    google::InitGoogleLogging(argc > 0 ? argv[0] : nullptr);
     LOG(INFO)    << "info";
     LOG(WARNING) << "warning";
     LOG(ERROR)   << "error";
-    LOG(FATAL)   << "fatal";
+    // 输出堆栈
+    // LOG(FATAL)   << "fatal";
+    VLOG(0)      << "info";
+    VLOG(1)      << "warning";
     google::FlushLogFiles(google::GLOG_INFO);
     google::FlushLogFiles(google::GLOG_WARNING);
     google::FlushLogFiles(google::GLOG_ERROR);
