1492e37b1057aa661f9219b32b7af2a764274428
acgist <289547414@qq.com>
2024-04-05 09:42:20 +0800

    [+] GPU

--- boot/test/src/TestLibTorchGenderHandler.cpp
+++ boot/test/src/TestLibTorchGenderHandler.cpp
@@ -3,19 +3,22 @@
 int main(const int argc, const char * const argv[]) {
     lifuren::logger::init();
     SPDLOG_DEBUG("测试");
-    lifuren::Gender gener;
+    lifuren::Gender gender;
     lifuren::GenderHandler handler;
-    handler.model = gener;
+    handler.model = gender;
+    if(torch::cuda::is_available()) {
+        handler.device = torch::Device(torch::kCUDA);
+    }
     #ifdef _WIN32
-    // handler.trainAndVal(
-    //     32,
-    //     8,
-    //     0.001,
-    //     "D:\\tmp\\gender",
-    //     ".jpg",
-    //     "D:\\tmp\\gender\\model.pt"
-    // );
-    handler.load("D:\\tmp\\gender\\model.pt");
+    handler.trainAndVal(
+        32,
+        8,
+        0.001,
+        "D:\\tmp\\gender",
+        ".jpg",
+        "D:\\tmp\\gender\\model.pt"
+    );
+    // handler.load("D:\\tmp\\gender\\model.pt");
     cv::Mat image = cv::imread("D:\\tmp\\yusheng.jpg");
     SPDLOG_DEBUG("预测结果：{}", handler.pred(image));
     image.release();
@@ -23,14 +26,14 @@ int main(const int argc, const char * const argv[]) {
     SPDLOG_DEBUG("预测结果：{}", handler.pred(image));
     image.release();
     #else
-    // handler.trainAndVal(
-    //     32,
-    //     8,
-    //     0.001,
-    //     "/tmp/gender",
-    //     ".jpg",
-    //     "/tmp/gender/model.pt"
-    // );
+    handler.trainAndVal(
+        32,
+        8,
+        0.001,
+        "/tmp/gender",
+        ".jpg",
+        "/tmp/gender/model.pt"
+    );
     #endif
     SPDLOG_DEBUG("完成");
     lifuren::logger::shutdown();
