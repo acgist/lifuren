84ec5c53d78cb9a43d94aacc763b500a684d17f1
acgist <289547414@qq.com>
2024-06-27 11:22:16 +0800

    [*] matplot

--- boot/test/src/source/matplot/TestMatplot.cpp
+++ boot/test/src/source/matplot/TestMatplot.cpp
@@ -1,6 +1,9 @@
 #include "matplot/matplot.h"
 
 int main() {
+    auto plot = matplot::figure();
+    plot->name("acgist");
+    plot->number_title(false);
     matplot::hold(matplot::on);
     std::vector<double> x = matplot::linspace(0, 2 * matplot::pi);
     std::vector<double> y = matplot::transform(x, [](auto x) {
@@ -10,6 +13,6 @@ int main() {
     matplot::plot(x, matplot::transform(y, [](auto y) {
         return -y;
     }), "--r");
-    matplot::show();
+    matplot::show(plot);
     return 0;
 }
