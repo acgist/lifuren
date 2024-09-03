84d774b1c6611ded961bc91264a64580241a8d93
acgist <289547414@qq.com>
2024-09-03 19:10:39 +0800

    [*] sd

--- boot/test/src/source/cv/TestStableDiffusionCPPPaintClient.cpp
+++ boot/test/src/source/cv/TestStableDiffusionCPPPaintClient.cpp
@@ -4,11 +4,12 @@
 
 #include "spdlog/spdlog.h"
 
-static void testSD(const std::string& image, const std::string& prompt) {
+static void testSD(const std::string& image, const std::string& prompt, const std::string& output) {
     lifuren::StableDiffusionCPPPaintClient client{};
     client.paint({
         .image  = image,
-        .prompt = prompt
+        .prompt = prompt,
+        .output = output
     }, [](bool finish, float percent, const std::string& message) {
         return true;
     });
@@ -17,9 +18,10 @@ static void testSD(const std::string& image, const std::string& prompt) {
 int main(const int argc, const char * const argv[]) {
     lifuren::logger::init();
     SPDLOG_DEBUG("测试");
-    std::string image  = argc > 1 ? argc[1] : "";
+    std::string output = argc > 2 ? argv[2] : "";
+    std::string image  = argc > 1 ? argv[1] : "";
     std::string prompt = argc > 0 ? argv[0] : "flower";
-    testSD(image, prompt);
+    testSD(image, prompt, output);
     SPDLOG_DEBUG("完成");
     lifuren::logger::shutdown();
     return 0;
