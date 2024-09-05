690cb2b231000bff0f43c679bcb7ac3f12657f6d
acgist <289547414@qq.com>
2024-09-05 08:27:52 +0800

    [*] 优化

--- boot/test/src/source/nlp/TestFaiss.cpp
+++ boot/test/src/source/nlp/TestFaiss.cpp
@@ -4,12 +4,14 @@
 
 #include "lifuren/Logger.hpp"
 
+using idx_t = int64_t;
+
 static void testSearch() {
     faiss::IndexFlatL2 db(3);
     float a[]{1.0, 2.0, 3.0};
     db.add(1, a);
     int k = 5;
-    faiss::idx_t* I = new faiss::idx_t[k * 5];
+    idx_t* I = new idx_t[k * 5];
     float* D = new float[k * 5];
     db.search(5, a, k, D, I);
 }
