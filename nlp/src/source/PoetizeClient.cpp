714dfd2744876fe62ec2a9ac84fe3688aa22a546
acgist <289547414@qq.com>
2024-09-11 17:30:45 +0800

    [*] 编译参数

--- nlp/src/source/PoetizeClient.cpp
+++ nlp/src/source/PoetizeClient.cpp
@@ -13,19 +13,19 @@ bool lifuren::PoetizeClient::stop() {
 std::unique_ptr<lifuren::PoetizeClient> lifuren::PoetizeClient::getClient(const std::string& client) {
     if(client == "poetize-shifo-rnn") {
         return std::make_unique<lifuren::ShifoRNNPoetizeClient>();
-    if(client == "poetize-shimo-rnn") {
+    } else if(client == "poetize-shimo-rnn") {
         return std::make_unique<lifuren::ShimoRNNPoetizeClient>();
-    if(client == "poetize-shigui-rnn") {
+    } else if(client == "poetize-shigui-rnn") {
         return std::make_unique<lifuren::ShiguiRNNPoetizeClient>();
-    if(client == "poetize-shixian-rnn") {
+    } else if(client == "poetize-shixian-rnn") {
         return std::make_unique<lifuren::ShixianRNNPoetizeClient>();
-    if(client == "poetize-shisheng-rnn") {
+    } else if(client == "poetize-shisheng-rnn") {
         return std::make_unique<lifuren::ShishengRNNPoetizeClient>();
-    if(client == "poetize-lidu-rnn") {
+    } else if(client == "poetize-lidu-rnn") {
         return std::make_unique<lifuren::LiduRNNPoetizeClient>();
-    if(client == "poetize-suxin-rnn") {
+    } else if(client == "poetize-suxin-rnn") {
         return std::make_unique<lifuren::SuxinRNNPoetizeClient>();
-    if(client == "poetize-wanyue-rnn") {
+    } else if(client == "poetize-wanyue-rnn") {
         return std::make_unique<lifuren::WanyueRNNPoetizeClient>();
     } else {
         return nullptr;
